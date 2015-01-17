3fafe69788b62fc12182769bba580780bda5df7a
Petr Zuev <zps.petr@gmail.com>
2015-01-18 00:02:51 +0600

    optimazing function show_item

--- lesson4.php
+++ lesson4.php
@@ -21,6 +21,7 @@ diskont = diskont' . mt_rand(0, 2) . ';
 
 ';
 $bd = parse_ini_string($ini_string, true);
+var_dump($bd);
 
 function draw_line() {
     echo '<br>______________________________________________________________________________________________<br>';
@@ -43,7 +44,6 @@ function show_result() {
 $special_offer_conditions = array(
     'name' => 'игрушка детская велосипед',
     'количество заказано' => 3,
-    'осталось на складе' => 3
 );
 $special_offer_value = 30;
 $special_offer_text = 'Скидка по акции: "купи 3 и больше детских велосипедов и получи скидку 30%!"';
@@ -54,11 +54,9 @@ function check_offer($item) {
     if($special_offer_conditions['name'] != $item['name']) {
         return false;
     }
-    foreach ($special_offer_conditions as $key => $value) {
-        if ($key != 'name' && $item['params'][$key] < $value) {
+        if ($item['params']['количество заказано'] < $special_offer_conditions['количество заказано']) {
                 return false;
             }
-    }
 
     return true;
 }
@@ -83,53 +81,36 @@ function show_item($item, $stock) {
     }
     $cur_discount_price = $cur_price - $cur_price * $cur_discount / 100;
 
-    //Товар в наличии в полном объеме
-    if ($stock >= 0) {
-        echo '<br>' . $count . '. ' . $item['name'] . '<br>Количество:' . $cur_offer . ' шт. по цене: ' . $cur_price . ' руб.';
+    //Товар есть
+    if ($cur_stock > 0) {
+        $pc = ($cur_offer > $cur_stock)?$cur_stock:$cur_offer;
+        echo '<br>' . $count . '. ' . $item['name'] . '<br>Количество:' . $pc . ' шт. по цене: ' . $cur_price . ' руб.';
         //есть скидка на товар
         if ($cur_discount > 0) {
-            echo '<br>Скидка:' . $cur_discount . '% сумма со скидкой:' . ($cur_discount_price * $cur_offer) . ' руб.';
-            echo (check_offer($item) == true) ? '<br>' . $special_offer_text : '';
-            $result['sum']+= ($cur_discount_price * $cur_offer);
+            echo '<br>Скидка:' . $cur_discount . '% сумма со скидкой:' . ($cur_discount_price * $pc) . ' руб.';
+            echo (check_offer($item) == true && $cur_stock > 3) ? '<br>' . $special_offer_text : '';
+            $result['sum']+= ($cur_discount_price * $pc);
         }
         //Нет скидки
         else {
-            echo '<br>Сумма:' . ($cur_price * $cur_offer) . ' руб.';
-            $result['sum']+= ($cur_price * $cur_offer);
+            echo '<br>Сумма:' . ($cur_price * $pc) . ' руб.';
+            $result['sum']+= ($cur_price * $pc);
+        }
+        if($cur_stock<$cur_offer) {
+              echo '<br><b>ВНИМАНИЕ!</b> Товара не хватает на складе! Недостающее количество: ' . $cur_out_stock;
         }
         $result['item_names']+=1;
-        $result['quantity']+=$cur_offer;
+        $result['quantity']+=$pc;
     }
-    //Товара не хватает
-    else {
-        if ($cur_out_stock < $cur_offer) {
-            echo '<br>' . $count . '. ' . $item['name'] . '<br>Количество:' . $cur_stock . ' шт. по цене: ' . $cur_price . ' руб.';
-            //Есть скидки
-            if ($cur_discount > 0) {
-                echo '<br>Скидка:' . $cur_discount . '% сумма со скидкой:' . ($cur_discount_price * $cur_stock) . ' руб.';
-                echo (check_offer($item) == true) ? '<br>' . $special_offer_text : '';
-                $result['sum']+= ($cur_discount_price * $cur_stock);
-            }
-            //Нет скидки
-            else {
-                echo '<br>Сумма:' . ($cur_price * $cur_stock) . ' руб.<br>';
-                $result['sum']+= ($cur_price * $cur_stock);
-            }
-            echo '<br><b>ВНИМАНИЕ!</b> Товара не хватает на складе! Недостающее количество: ' . $cur_out_stock;
-
-            $result['item_names']+=1;
-            $result['quantity']+=$cur_stock;
-        }
         //Товар отсустствует на складе
         else {
             echo '<br>' . $count . '. ' . $item['name'] . '<br>Количество:' . $cur_offer . ' шт. по цене: ' . $cur_price . ' руб.';
             echo '<br><b>ВНИМАНИЕ!</b> Извините, в данный момент товара нет в наличии на складе! Попробуйте заказать его позднее!';
         }
-    }
     draw_line();
     $count++;
 }
-
+   
 function show_cart() {
     global $bd;
     echo 'Состав вашего заказа №' . rand(0, 100) . ':<br>';
